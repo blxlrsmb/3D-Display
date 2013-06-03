30948c28437ef04562f15d3573625ab1b550e9e9
jiakai <jia.kai66@gmail.com>
2013-06-04 00:57:16 +0800

    demo cycle_signal usage

--- src/main.v
+++ src/main.v
@@ -1,16 +1,30 @@
 /*
  * $File: main.v
- * $Date: Sat Jun 01 20:34:44 2013 +0800
+ * $Date: Tue Jun 04 00:52:42 2013 +0800
  * $Author: jiakai <jia.kai66@gmail.com>
  */
 
 module main(
-	output [0:15] row, col,
-	input clock);
+	output reg [0:15] row, col,
+	input clock, cycle_marker);
 
 	wire [0:255] data = 255'h0FF03FFC781E700EE007C003C003C003C003C003C003E007700E781E3FFC0FF0;
 
-	disp_matrix disp_matrix(data, clock, row, col);
+	wire [0:15] row0, col0;
+	reg enable;
+
+	disp_matrix disp_matrix(data, clock, row0, col0);
+
+	always @(posedge clock) begin
+		if (cycle_marker) begin
+			row <= row0;
+			col <= col0;
+		end
+		else begin
+			row <= 0;
+			col <= 0;
+		end
+	end
 
 endmodule
 
